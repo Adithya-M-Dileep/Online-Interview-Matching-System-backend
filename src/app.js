302078d96f616bc456c7bcadb9ce8ab1cb8f6b67
Adithya M Dileep <73463333+Adithya-M-Dileep@users.noreply.github.com>
2023-11-24 00:09:55 +0530

    added ai_room chat

--- src/app.js
+++ src/app.js
@@ -1,8 +1,39 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import OpenAI from "openai";
 
+
+dotenv.config();
+const openai = new OpenAI();
 const app = express();
 
 // // Middleware
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
+openai.apiKey=process.env.OPENAI_API_KEY;
+
+// Handle POST requests
+app.post('/api/aimockinterview', async (req, res) => {
+  const clientMessages = req.body.messages;
+
+  const serverReply=await getOpenaiResponse(clientMessages);
+
+  // Send a response
+
+  res.json({ message: serverReply});
+});
+
+async function getOpenaiResponse(messages) {
+  try{
+    const completion = await openai.chat.completions.create({
+      messages: messages,
+      model: "gpt-3.5-turbo",
+    });
+
+    return completion.choices[0];
+  }catch(err){
+    console.log(err.message);
+  }
+}
 
 export default app;
