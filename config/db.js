78d1ba5c5930f5dcd1319005e89d51df58043112
Adithya M Dileep <73463333+Adithya-M-Dileep@users.noreply.github.com>
2023-11-25 12:20:25 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -9,6 +9,12 @@ const connectDB = async () => {
     const password=process.env.MONGODB_PASSWORD;
     await mongoose.connect('mongodb+srv://'+username+':'+password+'@oims-db-cluster.vftasnd.mongodb.net/OIMS');
     console.log('MongoDB connected');
+
+    // Accessing a specific collection and dropping it
+    const connection = mongoose.connection;
+    connection.dropCollection('CurrentUser');
+    connection.dropCollection('PairedConnections');
+    console.log('Collection cleared')
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
     // You might want to handle this error in a different way based on your application's needs
